063cee37273a8fcd58a2efd9b769c6de32e4cca6
Tayyaba10 <159796040+Tayyaba10@users.noreply.github.com>
2024-04-18 19:34:46 +0500

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//import the inquirer module
+import inquirer from "inquirer";
+//declare a constant answer and assign to it 
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words :"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+//print the array of words 
+console.log(words);
+//print the word count of the sentence
+console.log(`Your sentence word count is ${words.length}`);
